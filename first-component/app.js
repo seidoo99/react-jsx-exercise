dfbf43654ce2162004d47fb9f27dfd22ba0b8a7d
seid zerihun <seidoo99@gmial.com>
2020-06-04 22:57:06 -0400

    first-compnent

--- first-component/app.js
+++ first-component/app.js
@@ -1,7 +1,7 @@
 let FirstComponent = () => {
     // return <span> Make this component render like the Windows Logo: <a href = "https://goo.gl/images/mY5Qdv"
     // target = "_blank" > https: //goo.gl/images/mY5Qdv</a></span>
-    <div className='windows_logo'>
+    return <div className='windows_logo'>
 			<div style={{ backgroundColor: '#F36523' }}></div>
 			<div style={{ backgroundColor: '#8DC73F' }}></div>
 			<div style={{ backgroundColor: '#00ADEF' }}></div>
